c1d3daf7676c07bb62d9c905c25a66520459b3fc
AllStores <allstores3@allstores.in>
2015-04-17 01:37:10 +0530

    Revert "search"
    This reverts commit a828dad4154243cc0ab7038d3a25e980c1eeb8b5.

--- storesconstants.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-//define("metadata_query","select merchant_name from stores_metadata where merchant_name like '%"."SEARCH_KEYWORD"."%'");  Earlier query 
-
-define("metadata_query","select * from stores_metadata ");
-
-?>
